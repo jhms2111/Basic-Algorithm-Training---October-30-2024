a79108a4f01a3ac7e8d4055bc08aeada62f1f0ae
Joao <joao@appsimplantadores.com>
2024-10-30 23:40:54 +0100

    clase3/clase4

---
+++ clase3/ejercicio3.js
@@ -0,0 +1,8 @@
+
+
+
+    let mensagem = "Hola, mundo"
+
+    let novaMensagem = mensagem.replace("mundo", "JavaScript")
+
+    console.log(novaMensagem)
---
+++ clase4/ejercicio4.js
@@ -0,0 +1,5 @@
+let texto = "Desenvolvimento Web"
+
+let subTexto = texto.substring(0, 15)
+
+console.log(subTexto)
